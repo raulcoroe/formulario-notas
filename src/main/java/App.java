9a0aaf2f3fb788f4d4c277b9ce48d6f7e918fd5b
Raul2 <rmartinc96@gmail.com>
2022-03-24 18:23:34 +0100

    formi

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,22 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import util.R;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(R.getUI("formulario.fxml"));
+        loader.setController(this);
+        VBox vBox = loader.load();
+
+        Scene scene = new Scene(vBox);
+        stage.setScene(scene);
+        stage.setTitle("Formulario");
+        stage.show();
+    }
 }
